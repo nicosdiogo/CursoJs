a0d6d908bbef60e1ba32d7da8ee192b2029afe2f
Nicola D. João <nicosdiogo@gmail.com>
2026-01-28 16:34:04 +0100

    Tarefa Concluida

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,5 +1,4 @@
-    
-    function performOperation() {
+function performOperation() {
     // Get user input from input fields
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
@@ -11,21 +10,25 @@
                     // Display the result
                     displayResult(result);
                 } else {
-                    displayResult('Please enter valid numbers');
+                    displayResult('Por Favor, insira números válidos.');
                 }
             }
 
             function multiply(a, b) {
                 // Introduce a debugger statement to pause execution
-                debugger;
-
-                // Multiply the numbers
-                return a * b;
+                //debugger;
+                    let add = a + b;
+                    let sub = a - b;
+                    let mult = a * b;
+                    let div = a / b;
+               
+                    // Multiply the numbers
+                return [add, sub, mult, div];
             }
 
             function displayResult(result) {
                 // Display the result in the paragraph element
                 const resultElement = document.getElementById('result');
-                resultElement.textContent = `The result is: ${result}`;
+                resultElement.innerHTML = `O Resultado é: <b>Adição:</b> ${result[0]}, <b>Subtração:</b> ${result[1]}, <b>Multiplicação:</b> ${result[2]}, <b>Divisão:</b> ${result[3]}`;
             }
         
